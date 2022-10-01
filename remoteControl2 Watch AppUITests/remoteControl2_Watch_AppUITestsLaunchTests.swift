0c51c706bd5d3ef5fef512c43925229814aad98b
Rnadint <rnadint@gmail.com>
2022-10-01 16:41:44 -0700

    Initial Commit

---
+++ remoteControl2 Watch AppUITests/remoteControl2_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  remoteControl2_Watch_AppUITestsLaunchTests.swift
+//  remoteControl2 Watch AppUITests
+//
+//  Created by Rnadint's Macbook on 10/1/22.
+//
+
+import XCTest
+
+final class remoteControl2_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ remoteControl2/remoteControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  remoteControl2App.swift
+//  remoteControl2
+//
+//  Created by Rnadint's Macbook on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct remoteControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
